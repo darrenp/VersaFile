1168bc924bf99ae15599135b2505672be54a7fbf
AaronS <aarons@rkosolutions.com>
2012-06-22 11:27:47 -0600

    Build V2.06.2202

--- app/models/view_definition.rb
+++ app/models/view_definition.rb
@@ -159,6 +159,7 @@ class ViewDefinition < ActiveRecord::Base
         :created_by => self.created_by,
         :updated_at => self.updated_at,
         :updated_by => self.updated_by,
+        :is_template => self.is_template,
         :cell_definitions => []
     }
 
@@ -212,6 +213,7 @@ private
       view_definition.created_at = data['created_at'] unless data['created_at'].nil?
       view_definition.updated_by = data['updated_by'] unless data['updated_by'].nil?
       view_definition.updated_at = data['updated_at'] unless data['updated_at'].nil?
+      view_definition.is_template = data['is_template'] unless data['is_template'].nil?
 
       view_definition.cell_definitions.destroy_all
       unless data['cell_definitions'].nil?
