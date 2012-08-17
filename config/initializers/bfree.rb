cd6a20bcb8df81ffb6547eee49dbb39520ed1f52
AaronS <aarons@rkosolutions.com>
2012-08-17 13:28:17 -0700

    Build v2.12.08.1601

--- config/initializers/bfree.rb
+++ config/initializers/bfree.rb
@@ -7,7 +7,7 @@ require 'prawn'
 configatron.bfree.major_version = 2
 configatron.bfree.minor_version = 12
 configatron.bfree.revision_number = 8
-configatron.bfree.build_number = 1601
+configatron.bfree.build_number = 1701
 
 configatron.dojo.version = ((Rails.env == 'development') ? '1.6.1.d' : '1.6.1.p')
 configatron.mobile.dojo.version = ((Rails.env == 'development') ? '1.7.2.d' : '1.7.2.p')
